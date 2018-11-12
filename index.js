58220643b8b5a9c250fb971d0f139fd21e0125a3
jesusg1012 <jesus.jg.medina@gmailcom>
2018-11-12 01:06:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ function takeANumber (lin, name)
 function nowServing(lin)
 {
   if(lin.length === 0)
-  {
     return "There is nobody waiting to be served!";
-  }
   var name = lin[0];
   lin.shift();
   return name;
 }
 function currentLine(lin)
 {
+  if(lin.length === 0)
+    return "The line is currently empty";
+  var line = "The line is currently: ";
   for(var x = 0; x < lin.length; x++)
   {
     
