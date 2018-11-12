c311a5216dc44ef35f8368c56fac9ce1648a24f3
jesusg1012 <jesus.jg.medina@gmailcom>
2018-11-12 01:00:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function takeANumber (lin, name)
 {
   lin.push(name);
-  return "Welcome, " + name + ". You are number " + lin.length + " in line."
+  return "Welcome, " + name + ". You are number " + lin.length + " in line.";
 }
-function nowServing()
+function nowServing(lin)
+{
+  if(lin.length === 0)
+  {
+    return "There is nobody waiting to be served!";
+  }
+  var name = lin[0];
+  lin.shift();
+  return name;
+}
+function currentLine(lin)
+{
+  for(var x = 0; x < lin.length; x++)
+  {
+    
+  }
+}
